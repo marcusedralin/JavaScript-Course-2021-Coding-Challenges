9aec94a56edbd6dad16f6b4776d02067cbac16f5
Marcus Edralin <medralin36@yahoo.com>
2021-05-11 14:11:37 -0700

    Coding Challenge 4

--- script.js
+++ script.js
@@ -127,52 +127,76 @@ Test data:
 */
 
 // Part 1 Show Average Scores //
-const dolphinsAverage = (96 + 108 + 89) / 3;
-const koalasAverage = (88 + 91 + 110) / 3;
+// const dolphinsAverage = (96 + 108 + 89) / 3;
+// const koalasAverage = (88 + 91 + 110) / 3;
 
-// Part 2 Calculate winner and log to the console. //
-const averageWinner = () => {
-    if (dolphinsAverage > koalasAverage) {
-        console.log(`Dolphins have the higher average score of ${dolphinsAverage}`)
-    } else if (koalasAverage > dolphinsAverage) {
-        console.log(`Koalas have the higher average score of ${koalasAverage}`);
-    } else {
-        console.log('They tied with the same average score.')
-    }
-}
+// // Part 2 Calculate winner and log to the console. //
+// const averageWinner = () => {
+//     if (dolphinsAverage > koalasAverage) {
+//         console.log(`Dolphins have the higher average score of ${dolphinsAverage}`)
+//     } else if (koalasAverage > dolphinsAverage) {
+//         console.log(`Koalas have the higher average score of ${koalasAverage}`);
+//     } else {
+//         console.log('They tied with the same average score.')
+//     }
+// }
 
-averageWinner();
+// averageWinner();
 
-// BONUS 1
+// // BONUS 1
 
-const dolphinsScore = 97 + 112 + 101;
-const koalasScore = 109 + 95 + 123;
+// const dolphinsScore = 97 + 112 + 101;
+// const koalasScore = 109 + 95 + 123;
 
-const calcAverage = (score) => {
-    const avg = score / 3;
-    return avg;
-}
+// const calcAverage = (score) => {
+//     const avg = score / 3;
+//     return avg;
+// }
 
-const dolphAvg = calcAverage(dolphinsScore);
-console.log(dolphAvg);
+// const dolphAvg = calcAverage(dolphinsScore);
+// console.log(dolphAvg);
 
-const koaAvg = calcAverage(koalasScore);
-console.log(koaAvg);
+// const koaAvg = calcAverage(koalasScore);
+// console.log(koaAvg);
 
-if (dolphAvg >= 100 && koaAvg >= 100) {
-    if (dolphAvg > koaAvg) {
-        console.log(`Dolphins win: ${dolphAvg}.`);
-    } else if (koaAvg > dolphAvg) {
-        console.log(`Koalas win: ${koaAvg}`);
-    } else {
-        console.log('They tied!');
-    }
-} else {
-    console.log("Competitor(s) didn't score over 100.")
-}
+// if (dolphAvg >= 100 && koaAvg >= 100) {
+//     if (dolphAvg > koaAvg) {
+//         console.log(`Dolphins win: ${dolphAvg}.`);
+//     } else if (koaAvg > dolphAvg) {
+//         console.log(`Koalas win: ${koaAvg}`);
+//     } else {
+//         console.log('They tied!');
+//     }
+// } else {
+//     console.log("Competitor(s) didn't score over 100.")
+// }
 
 // I created a function to calculate the average scores. 
 // Then I created an if block to define that both avg scores were greater than or equal to 100. If so it goes into the comparison between the PASSED scores over 100.
 // Otherwise it says they both didn't score high enough.
 
+// Coding Challenge #4 //
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement � (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary 
+operator!)
+
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+
+Test data:
+
+§ Data 1: Test for bill values 275, 40 and 430
+
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 �
+*/
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill is: $${bill}, the tip was: $${tip} and the total was: $${bill + tip}`);
 
