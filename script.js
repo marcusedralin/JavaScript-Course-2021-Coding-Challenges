b38b8fad28474a1d83d6e7d6de28250efb9d23bc
Marcus Edralin <medralin36@yahoo.com>
2021-05-13 13:16:33 -0700

    JS Fundamentals Part 2-Coding Challenge #2

--- script.js
+++ script.js
@@ -235,30 +235,69 @@ Hints:
 Apply this to the team's average scores
 */
 
-function addScores(a, b, c) {
-    const totalScores = a + b + c;
-    return totalScores;
+// function addScores(a, b, c) {
+//     const totalScores = a + b + c;
+//     return totalScores;
+// }
+
+// // const dolphinsScores = 44 + 23 + 71;
+// const dolphinsScores = addScores(85, 54, 41);
+// // const koalasScores = 65 + 54 + 49;
+// const koalasScores = addScores(23, 34, 27);
+
+// const calcAverage = (scores) => scores / 3;
+
+// const dolphAvg = calcAverage(dolphinsScores);
+// const koaAvg = calcAverage(koalasScores);
+
+// function checkWinner(dolphAvg, koaAvg) {
+//     if (dolphAvg >= 2 * koaAvg) {
+//         console.log(`Dolphins win (${dolphAvg} vs. ${koaAvg})`);
+//     } else if (koaAvg >= 2 * dolphAvg) {
+//         console.log(`Koalas win (${koaAvg} vs. ${dolphAvg})`);
+//     } else {
+//         console.log('No one wins...')
+//     }
+// }
+
+// checkWinner(dolphAvg, koaAvg);
+
+// Coding Challenge #2 //
+
+/*
+Coding Challenge #2
+
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+20%.
+
+Your tasks:
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+
+2. And now let's use arrays! So create an array 'bills' containing the test data below
+
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+Test data: 125, 555 and 44
+
+Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 
+*/
+
+const calcTip = (bill) => {
+    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+    return tip;
 }
 
-// const dolphinsScores = 44 + 23 + 71;
-const dolphinsScores = addScores(85, 54, 41);
-// const koalasScores = 65 + 54 + 49;
-const koalasScores = addScores(23, 34, 27);
+const bills = [125, 555, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
 
-const calcAverage = (scores) => scores / 3;
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(total);
 
-const dolphAvg = calcAverage(dolphinsScores);
-const koaAvg = calcAverage(koalasScores);
 
-function checkWinner(dolphAvg, koaAvg) {
-    if (dolphAvg >= 2 * koaAvg) {
-        console.log(`Dolphins win (${dolphAvg} vs. ${koaAvg})`);
-    } else if (koaAvg >= 2 * dolphAvg) {
-        console.log(`Koalas win (${koaAvg} vs. ${dolphAvg})`);
-    } else {
-        console.log('No one wins...')
-    }
-}
 
-checkWinner(dolphAvg, koaAvg);
 
