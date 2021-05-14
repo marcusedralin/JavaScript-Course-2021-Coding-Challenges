94aff9de77ece1a8fff8aa7cd7c357439ffc8bd0
Marcus Edralin <medralin36@yahoo.com>
2021-05-13 18:41:39 -0700

    JS Fundamentals Part 2-Coding Challenge # 3

--- script.js
+++ script.js
@@ -285,18 +285,75 @@ Test data: 125, 555 and 44
 Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 
 */
 
-const calcTip = (bill) => {
-    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-    return tip;
+// const calcTip = (bill) => {
+//     const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//     return tip;
+// }
+
+// const bills = [125, 555, 44];
+// console.log(bills);
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(tips);
+
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+// console.log(total);
+
+// Coding Challenge # 3 //
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
+implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter)
+
+Your tasks:
+
+1. For each of them, create an object with properties for their full name, mass, and 
+height (Mark Miller and John Smith)
+
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
+method on both objects). Store the BMI value to a property, and also return it 
+from the method
+
+3. Log to the console who has the higher BMI, together with the full name and the 
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m 
+tall.
+
+GOOD LUCK 
+*/
+
+const mark = {
+    firstName: 'Mark',
+    lastName: 'Miller',
+    mass: 78,
+    height: 1.69,
+    calcBmi: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi;
+    }
 }
 
-const bills = [125, 555, 44];
-console.log(bills);
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(tips);
+const john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    mass: 92,
+    height: 1.95,
+    calcBmi: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi;
+    }
+}
 
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
-console.log(total);
+mark.calcBmi();
+john.calcBmi();
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.firstName + ' ' + mark.lastName}'s BMI (${mark.bmi}) is higher than ${john.firstName}'s (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.firstName + ' ' + john.lastName}'s BMI (${john.bmi}) is higher than ${mark.firstName}'s (${mark.bmi})`)
+} else {
+    console.log('They have the same BMI...')
+}
 
 
 
